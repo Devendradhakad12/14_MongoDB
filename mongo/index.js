dfb6f5d73ce56156b5f7a335b5d36ac7c94745fe
Devendra Dhakad <121676745+Devendradhakad12@users.noreply.github.com>
2023-09-15 08:53:51 +0530

    updation

--- mongo/index.js
+++ mongo/index.js
@@ -21,24 +21,28 @@ const UserSchema = new mongoose.Schema({
   },
   age: {
     type: Number,
-    required:true
+    required:true,
+    min:[18,"Age must be greater then 18"]
   },
 });
 const User = mongoose.model("User",UserSchema)
 
-const user1 = new User({name:"tony",email:"tony@gmail.com",age:"17"})
+const user1 = new User({name:"steve",email:"steve@gmail.com",age:"150"})
 //user1.save().then(()=>{console.log("user created")}).catch((err)=>console.log(err))
-
-const users = User.find({age:{$lt:18}})
- users.then((res)=>console.log(res)).catch((err)=>console.log(err))
+ 
+//const users = User.find({age:{$lt:18}})
+const users = User.find({})
+// users.then((res)=>console.log(res)).catch((err)=>console.log(err))
 
  //User.updateOne({_id:"6502a8a36fa6f2b82ae57267"},{age:16}).then((res)=>console.log(res)).catch((err)=>console.log(err))
 // User.updateOne({age:{$gt:18}},{age:16}).then((res)=>console.log(res)).catch((err)=>console.log(err))
 
-User.findOneAndUpdate({name:"mahak"},{age:20},{new:true}).then((res)=>console.log(res)).catch((err)=>console.log(err))
-User.findOneAndDelete({_id:"6502a52d1366d20a63d7a052"}).then((res)=>console.log(res)).catch((err)=>console.log(err))
+//User.findOneAndUpdate({name:"mahak"},{age:20},{new:true}).then((res)=>console.log(res)).catch((err)=>console.log(err))
+//User.findOneAndDelete({_id:"6502a52d1366d20a63d7a052"}).then((res)=>console.log(res)).catch((err)=>console.log(err))
 
+//? update with validators
+User.findByIdAndUpdate("6503cb298edf0edd896bc4b1",{age:17},{runValidators:true}).then((res)=>console.log(res)).catch((err)=>console.log(err.errors.age.message))
   
  
 connectToDB(); 
-  
+   
